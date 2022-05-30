a4c5e92b20b9afb00a9fec6755856324b3f9bf15
akshatbatra10 <62565788+akshatbatra10@users.noreply.github.com>
2022-05-30 17:36:23 +0530

    Max Consecutive Ones solution

---
+++ LeetCode/Array/MaxConsecutiveOnes.java
@@ -0,0 +1,23 @@
+/* Question - Max Consecutive Ones */
+/* 
+  Given a binary array nums, return the maximum number of consecutive 1's in the array.
+*/
+
+/* Solution */
+
+class Solution {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int count = 0;
+        int maxCount = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 1) {
+                count++;
+            } else {
+                maxCount = Math.max(maxCount, count);
+                count = 0;
+            }
+        }
+        maxCount = Math.max(maxCount, count);
+        return maxCount;
+    }
+}
