7c4530bcbe65c02c576be9636abdc891ecf697ba
akshatbatra10 <62565788+akshatbatra10@users.noreply.github.com>
2022-08-01 21:36:58 +0530

    Range Sum Query 2D - Immutable - Solution

---
+++ LeetCode/Array/RangeSumQuery2D_Immutable.java
@@ -0,0 +1,37 @@
+/* Question - Range Sum Query 2D - Immutable */
+/* 
+  Given a 2D matrix matrix, handle multiple queries of the following type:
+
+  Calculate the sum of the elements of matrix inside the rectangle defined by its upper left corner (row1, col1) and lower right corner (row2, col2).
+  Implement the NumMatrix class:
+
+  NumMatrix(int[][] matrix) Initializes the object with the integer matrix matrix.
+  int sumRegion(int row1, int col1, int row2, int col2) Returns the sum of the elements of matrix inside the rectangle defined by its upper left corner (row1, col1) and lower right corner (row2, col2).
+*/
+
+/* Solution */
+
+class NumMatrix {
+    
+    int[][] dp;
+
+    public NumMatrix(int[][] matrix) {
+        if (matrix.length == 0 || matrix[0].length == 0) return;
+        dp = new int[matrix.length + 1][matrix[0].length + 1];
+        for (int r = 0; r < matrix.length; r++) {
+            for (int c = 0; c < matrix[0].length; c++) {
+                dp[r + 1][c + 1] = dp[r + 1][c] + dp[r][c + 1] + matrix[r][c] - dp[r][c];
+            }
+        }
+    }
+    
+    public int sumRegion(int row1, int col1, int row2, int col2) {
+        return dp[row2 + 1][col2 + 1] - dp[row1][col2 + 1] - dp[row2 + 1][col1] + dp[row1][col1];
+    }
+}
+
+/**
+ * Your NumMatrix object will be instantiated and called as such:
+ * NumMatrix obj = new NumMatrix(matrix);
+ * int param_1 = obj.sumRegion(row1,col1,row2,col2);
+ */
