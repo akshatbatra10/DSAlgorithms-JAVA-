5708be95ba3f15b315de7c4f8213d68377e7d415
akshatbatra10 <62565788+akshatbatra10@users.noreply.github.com>
2022-11-01 13:14:27 +0530

    Time: 1 ms (96.80%), Space: 43.6 MB (89.49%) - LeetHub

--- 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.java
+++ 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.java
@@ -4,12 +4,13 @@ class Solution {
         int m = grid.length;
         int n = grid[0].length;
         int[] ans = new int[n];
+        Integer[][] dp = new Integer[m][n];
         for (int j = 0; j < n; j++) {
-            ans[j] = find(grid, 0, j, m, n);
+            ans[j] = find(grid, 0, j, m, n, dp);
         }
         return ans;
     }
-    public int find(int[][] grid, int i, int j, int m, int n) {
+    public int find(int[][] grid, int i, int j, int m, int n, Integer[][] dp) {
         if (i == m) {
             return j;
         }
@@ -17,6 +18,9 @@ class Solution {
         if (next == -1 || next > n - 1 || grid[i][j] != grid[i][next]) {
             return -1;
         }
-        return find(grid, i + 1, next, m, n);
+        if (dp[i][j] != null) {
+            return dp[i][j];
+        }
+        return dp[i][j] = find(grid, i + 1, next, m, n, dp);
     }
 }
