0e9466793097cdd8439d5c5a1cde16f7bceb1c5a
Akshat Batra <62565788+akshatbatra10@users.noreply.github.com>
2022-11-09 17:40:20 +0530

    Time: 45 ms (96.16%), Space: 50.2 MB (97.35%) - LeetHub

---
+++ 0901-online-stock-span/0901-online-stock-span.java
@@ -0,0 +1,21 @@
+class StockSpanner {
+    
+    Stack<int[]> st;
+    
+    public StockSpanner() {
+        st = new Stack<>();
+    }
+    
+    public int next(int price) {
+        
+        int span = 1;
+        
+        while(st.size() > 0 && price >= st.peek()[0]){
+            span += st.pop()[1];
+        }
+        
+        st.push(new int[]{price, span});
+        
+        return span;
+    }
+}
