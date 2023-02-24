05682b36b1ac2978e02e642f44fefad60bdc47b2
Akshat Batra <62565788+akshatbatra10@users.noreply.github.com>
2023-02-24 19:30:05 +0530

    Time: 119 ms (84.91%), Space: 50.9 MB (47.17%) - LeetHub

--- 1675-minimize-deviation-in-array/1675-minimize-deviation-in-array.java
+++ 1675-minimize-deviation-in-array/1675-minimize-deviation-in-array.java
@@ -1,23 +1,21 @@
 class Solution {
     public int minimumDeviation(int[] nums) {
-        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
-        int minVal = Integer.MAX_VALUE;
-        for (int num : nums) {
-            if (num % 2 == 1)
-                num = num * 2;
-            pq.offer(num);
-            minVal = Math.min(minVal, num);
-        }
-        int minDeviation = Integer.MAX_VALUE;
+        TreeSet<Integer> set = new TreeSet<>();
+        for (int x : nums)
+            if (x%2 == 0)
+                set.add(x);
+            else
+                set.add(x*2);
+        int ans = Integer.MAX_VALUE;
         while (true) {
-            int maxVal = pq.poll();
-            minDeviation = Math.min(minDeviation, maxVal - minVal);
-            if (maxVal % 2 == 1)
+            int val = set.last();
+            ans = Math.min(ans, val - set.first());
+            if (val%2 == 0) {
+                set.remove(val);
+                set.add(val/2);
+            } else
                 break;
-            maxVal = maxVal / 2;
-            minVal = Math.min(minVal, maxVal);
-            pq.offer(maxVal);
         }
-        return minDeviation;
+        return ans;
     }
 }
