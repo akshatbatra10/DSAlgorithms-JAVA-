475ca361b72432a6407d70bda1fff4fc25fca933
Akshat Batra <62565788+akshatbatra10@users.noreply.github.com>
2023-04-05 20:55:25 +0530

    Time: 9 ms (74.55%), Space: 62.6 MB (7.17%) - LeetHub

---
+++ 2439-minimize-maximum-of-array/2439-minimize-maximum-of-array.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int minimizeArrayValue(int[] nums) {
+        long answer = 0, prefixSum = 0;   
+
+        for (int i = 0; i < nums.length; ++i) {
+            prefixSum += nums[i];
+            answer = Math.max(answer, (prefixSum + i) / (i + 1));
+        }
+
+        return (int)answer;
+    }
+}
