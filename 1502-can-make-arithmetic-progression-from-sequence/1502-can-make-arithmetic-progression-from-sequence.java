b3861ad985dd32cebc2c82c96e22463575ba5963
Akshat Batra <62565788+akshatbatra10@users.noreply.github.com>
2023-06-06 21:33:42 +0530

    Time: 2 ms (81.19%), Space: 40.9 MB (91.81%) - LeetHub

--- 1502-can-make-arithmetic-progression-from-sequence/1502-can-make-arithmetic-progression-from-sequence.java
+++ 1502-can-make-arithmetic-progression-from-sequence/1502-can-make-arithmetic-progression-from-sequence.java
@@ -3,25 +3,33 @@ class Solution {
         int minValue = Arrays.stream(arr).min().getAsInt();
         int maxValue = Arrays.stream(arr).max().getAsInt();
         int n = arr.length;
-        
-        if (maxValue - minValue == 0) {
-            return true;
-        }
-        
         if ((maxValue - minValue) % (n - 1) != 0) {
             return false;
         }
         
         int diff = (maxValue - minValue) / (n - 1);
-        Set<Integer> numberSet = new HashSet<>();
+        int i = 0;
         
-        for (int i = 0; i < n; i++) {
-            if ((arr[i] - minValue) % diff != 0) {
+        while (i < n) {
+            if (arr[i] == minValue + i * diff) {
+                i += 1;
+                
+            } else if ((arr[i] - minValue) % diff != 0) {
                 return false;
+                
+            } else {
+                int j = (arr[i] - minValue) / diff;
+                
+                if (arr[i] == arr[j]) {
+                    return false;
+                }
+                
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
             }
-            numberSet.add(arr[i]);
         }
         
-        return numberSet.size() == n;
+        return true;
     }
 }
