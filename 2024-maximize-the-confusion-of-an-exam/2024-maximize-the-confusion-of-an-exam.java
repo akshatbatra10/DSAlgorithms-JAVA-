2584cde7c7b16fe43a76e687aad77f68c8cd5647
Akshat Batra <62565788+akshatbatra10@users.noreply.github.com>
2023-07-07 19:50:13 +0530

    Time: 361 ms (5.00%), Space: 44.4 MB (8.75%) - LeetHub

--- 2024-maximize-the-confusion-of-an-exam/2024-maximize-the-confusion-of-an-exam.java
+++ 2024-maximize-the-confusion-of-an-exam/2024-maximize-the-confusion-of-an-exam.java
@@ -1,23 +1,45 @@
 class Solution {
     public int maxConsecutiveAnswers(String answerKey, int k) {
-        int maxSize = k;
-        Map<Character, Integer> count = new HashMap<>();
-        for (int i = 0; i < k; i++) {
-            count.put(answerKey.charAt(i), count.getOrDefault(answerKey.charAt(i), 0) + 1);
+        int n = answerKey.length();
+        int left = k, right = n;
+        
+        while (left < right) {
+            int mid = (left + right + 1) / 2;
+            
+            if (isValid(answerKey, mid, k)) {
+                left = mid;
+            } else {
+                right = mid - 1;
+            }
         }
         
-        int left = 0;
-        for (int right = k; right < answerKey.length(); right++) {
-            count.put(answerKey.charAt(right), count.getOrDefault(answerKey.charAt(right), 0) + 1);
-            
-            while (Math.min(count.getOrDefault('T', 0), count.getOrDefault('F', 0)) > k) {
-                count.put(answerKey.charAt(left), count.get(answerKey.charAt(left)) - 1);
-                left++;
-            }
-            
-            maxSize = Math.max(maxSize, right - left + 1);
+        return left;
+    }
+    
+    private boolean isValid(String answerKey, int size, int k) {
+        int n = answerKey.length();
+        Map<Character, Integer> counter = new HashMap<>();
+        
+        for (int i = 0; i < size; i++) {
+            char c = answerKey.charAt(i);
+            counter.put(c, counter.getOrDefault(c, 0) + 1);
         }
-                    
-        return maxSize;
+        
+        if (Math.min(counter.getOrDefault('T', 0), counter.getOrDefault('F', 0)) <= k) {
+            return true;
+        }
+        
+        for (int i = size; i < n; i++) {
+            char c1 = answerKey.charAt(i);
+            counter.put(c1, counter.getOrDefault(c1, 0) + 1);
+            char c2 = answerKey.charAt(i - size);
+            counter.put(c2, counter.getOrDefault(c2, 0) - 1);
+            
+            if (Math.min(counter.getOrDefault('T', 0), counter.getOrDefault('F', 0)) <= k) {
+                return true;
+            }
+        }
+        
+        return false;
     }
 }
